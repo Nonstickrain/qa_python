1d6fd8f5c1580afbaa7cd7eeeb8b87363dcfaa43
Timonin Artyom <linkingsad@mail.ru>
2023-01-26 20:36:36 +0300

    First commit: added all tests

--- tests.py
+++ tests.py
@@ -1,24 +1,59 @@
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
 
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    def test_add_new_book_add_same_book(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert  len(collector.get_books_rating()) == 1
+    def test_set_book_rating_set_avarege_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Сантехник')
+        collector.set_book_rating('Сантехник', 5)
+        assert collector.books_rating['Сантехник'] == 5
+    def test_set_book_rating_set_default_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('Generation P')
+        assert collector.books_rating['Generation P'] == 1
+    def test_set_book_rating_set_min_invalid_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('По кругу на корпоративе')
+        collector.set_book_rating('По кругу на корпоративе', -3)
+        assert collector.books_rating['По кругу на корпоративе'] == 1
+
+    def test_set_book_rating_set_max_invalid_rating(self):
+        collector = BooksCollector()
+        collector.add_new_book('По кругу на вписке')
+        collector.set_book_rating('По кругу на вписке', 23)
+        assert collector.books_rating['По кругу на вписке'] == 1
+    def test_add_book_in_favorites_add_one_book(seld):
+        collector = BooksCollector()
+        collector.add_new_book('Соната')
+        collector.add_book_in_favorites('Соната')
+        assert len(collector.favorites) == 1
+    def test_add_book_in_favorites_add_not_existing_book(seld):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('Соната')
+        assert len(collector.favorites) == 0
+    def test_add_book_in_favorites_add_same_book(seld):
+        collector = BooksCollector()
+        collector.add_new_book('Соната')
+        collector.add_book_in_favorites('Соната')
+        collector.add_book_in_favorites('Соната')
+        assert len(collector.favorites) == 1
+    def test_delete_book_from_favorites_delete_one_book_from_favorities(seld):
+        collector = BooksCollector()
+        collector.add_new_book('Соната')
+        collector.add_book_in_favorites('Соната')
+        collector.delete_book_from_favorites('Соната')
+        assert len(collector.favorites) == 0
